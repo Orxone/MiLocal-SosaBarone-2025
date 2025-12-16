32d7e8636be9fa261e01879e2d38f0a2917cfaaa
God of Death <orianacsb@gmail.com>
2025-12-15 23:05:00 -0300

    Inicio de modo oscuro
    Inicio de modo oscuro, botón dark funcional, modificando pestaña de contacto, arreglada imagen promocional de descuendo en el index.

--- global.js
+++ global.js
@@ -1,3 +1,20 @@
+var theme = document.getElementById("modo-btn");
+
+if (localStorage.getItem("modo") === "oscuro"){
+    document.body.classList.add("dark-mode");
+}
+
+theme.onclick = function(){
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")){
+        localStorage.setItem("modo", "oscuro");
+    }
+
+    else{
+        localStorage.setItem("modo", "claro");
+    }
+}
+
 let burgerBtn = document.getElementById('burger-btn');
 let navbar = document.getElementById('nav-menu');
 
